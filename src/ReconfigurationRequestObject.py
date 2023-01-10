bb1f8e59aa9e8aed06eb5dd3c65e8ab7bc2454ec
T Vijay Prashant <55888513+tvijayprashant@users.noreply.github.com>
2023-01-10 23:13:47 +0530

    Delete src directory

--- src/ReconfigurationRequestObject.py
+++
@@ -1,7 +0,0 @@
-class ReconfigurationRequestObject:
-
-    def __init__(self, process):
-        self.process = process
-
-    def __str__(self):
-        return '[' + str(self.process) + ']'
--- src/State.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class State(Enum):
-    PENDING = 0
-    IMMUTABLE = 1
-    ACTIVE = 2
